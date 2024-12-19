14b184fc1ea9d609a75bd8272f47565fc61fa354
dwmanguiat <dwmanguiat@gmail.com>
2024-12-18 18:46:57 -0700

    ghostscript dependency

--- scripts/render_latex.py
+++ scripts/render_latex.py
@@ -4,21 +4,26 @@ import os
 def render_latex_to_pdf(latex_file):
     try:
         subprocess.run(['pdflatex', latex_file], check=True)
-        print(f"Successfully rendered {latex_file} to PDF.")
+        print(f"Successfully rendered {latex_file} to PDF. \n")
     except subprocess.CalledProcessError as e:
         print(f"Error rendering {latex_file} to PDF: {e}")
 
 def convert_pdf_to_png(pdf_file, output_png):
     try:
-        subprocess.run(['convert', '-density', '300', pdf_file, output_png], check=True)
+        subprocess.run(['magick', 'convert', '-density', '300', os.path.normpath(pdf_file), os.path.normpath(output_png)], check=True)
         print(f"Successfully converted {pdf_file} to {output_png}.")
     except subprocess.CalledProcessError as e:
         print(f"Error converting {pdf_file} to PNG: {e}")
 
 if __name__ == "__main__":
-    latex_file = 'resume.tex'
-    pdf_file = 'resume.pdf'
-    png_file = 'resume.png'
+    latex_file = os.path.abspath('resume.tex')
+    pdf_file = os.path.abspath('resume.pdf')
+    png_file = os.path.abspath('resume.jpg')
+
 
     render_latex_to_pdf(latex_file)
+
+    print(pdf_file)
+    print(png_file)
+
     convert_pdf_to_png(pdf_file, png_file)
